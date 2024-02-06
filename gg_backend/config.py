6036e22877c105a82d14bb36a2c9473157551b97
Ronny Lutz <ronny.lutz@studmail.htw-aalen.de>
2024-02-06 12:42:54 +0100

    DB aktulaisiert

--- gg_backend/config.py
+++ gg_backend/config.py
@@ -1,3 +1,3 @@
 # config.py
 
-DATABASE_URL = "postgresql://postgres:#Hamburger17@database-1.c1aicsgkk4rs.eu-north-1.rds.amazonaws.com:5432/database-1"
+DATABASE_URL = "postgresql://postgres:gaildorfgoals@database-1.c1aicsgkk4rs.eu-north-1.rds.amazonaws.com:5432/Gaildorf_Goals"
